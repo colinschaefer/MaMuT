0dbbacdc74b72abd697a3bbb6e9aa48a69a7cb64
Colin Schaefer <colin.schaefer@gmx.de>
2015-07-07 15:20:58 -0400

    Maximum projection is now rendered directly upon the first call

--- src/main/java/fiji/plugin/mamut/viewer/MamutVolumeRenderer.java
+++ src/main/java/fiji/plugin/mamut/viewer/MamutVolumeRenderer.java
@@ -1,32 +1,27 @@
 package fiji.plugin.mamut.viewer;
 
 import mpicbg.spim.data.generic.AbstractSpimData;
-import bdv.cl.RenderSlice;
 import bdv.cl.VolumeRenderer;
 import bdv.tools.brightness.BrightnessDialog;
 import bdv.tools.brightness.SetupAssignments;
 import bdv.tools.zdim.ZdimDialog;
 import bdv.viewer.InputActionBindings;
-import bdv.viewer.ViewerPanel;
 
 public class MamutVolumeRenderer extends VolumeRenderer {
-	RenderSlice render;
-	float currentdimZ = 20;
-	float minBright = 0;
-	float maxBright = 0;
-	ViewerPanel renderViewer;
-	SetupAssignments renderSetup;
-	ZdimDialog renderZdim;
-	BrightnessDialog renderBrightness;
-	boolean retimed = false;
 
 	public MamutVolumeRenderer(final AbstractSpimData<?> spimData,
 			final MamutViewerPanel viewer, final ZdimDialog zdimDialog,
 			final SetupAssignments setupAssignments,
 			final InputActionBindings bindings,
 			BrightnessDialog brightnessDialog) {
+
+		// construction with the superclass constructor
 		super(spimData, viewer, zdimDialog, setupAssignments, bindings,
 				brightnessDialog);
-	}
 
+		// initial rendering with setting of the right parameters
+		viewer.inverseMaxproj();
+		viewer.showMessage("maximum projection ON");
+		super.render();
+	}
 }
